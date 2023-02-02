8dcad24a6f12d58653d75f6314fac38acda131bf
JuanParraIV <jmparra1993@gmail.com>
2023-02-02 09:52:55 -0500

    feat: llenando base de datos con json Server a travez de script fakeapi

--- src/bulkcreate.js
+++ src/bulkcreate.js
@@ -1,22 +1,43 @@
-const { categories } = require('./categorias.json');
-const { instruments } = require('./instrumentos.json');
 const { Instrument, Category } = require("./libs/postgres");
+const axios = require("axios");
 
 
 const allData = async (req,res,next) => {
   try {
-    const resultQueryFromDb = await Category.findAll();
-    if (!resultQueryFromDb.length===0) {
-      const categoryArray = categories.map((category) => {
-        return {
-          name: category.name,
-        };
+    const getCategoriesFromJson = await axios.get(
+      `http://localhost:5050/categories/`);
+    const getinstrumentsFromJson = await axios.get(
+        `http://localhost:5050/instruments/`);
+      const categories =getCategoriesFromJson.data;
+      const instruments =getinstrumentsFromJson.data;
+
+      const categoryValidation = await Category.findOne({
+        where: { id: 1 },
       });
-      await Category.bulkCreate(categoryArray);
-      return res.status(200).json(categoryArray);
-    } else {
-      return res.status(200).json(resultQueryFromDb);
-    }
+      if (!categoryValidation) {
+        await Category.bulkCreate(categories);
+        console.log("Categories loaded in db succesfully");
+      }
+
+      const instValidation = await Instrument.findOne({
+        where: { id: 1 },
+      });
+
+      if (!instValidation) {
+        const inst = instruments.map(async (e) => {
+          await Instrument.create(e).then(async (x) => {
+            const category = e.category;
+            let newInstrumentCategory = await Category.findOne({
+              where: {
+                name: category,
+              },
+            });
+            await x.setCategory(newInstrumentCategory);
+          });
+        });
+
+        console.log("Instruments loaded in db succesfully");
+      }
 
   } catch (error) {
     console.log(error);
