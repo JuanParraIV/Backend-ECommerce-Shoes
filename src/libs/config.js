09f3380d4109bb6fd10966da41361589eccd717b
JuanParraIV <jmparra1993@gmail.com>
2023-02-03 21:53:10 -0500

    fix:Arreglando codigo para despliegue

--- src/libs/config.js
+++ src/libs/config.js
@@ -13,8 +13,8 @@ const ENV = {
   username: getEnvVariable('POSTGRES_USER'),
   password: getEnvVariable('POSTGRES_PASSWORD'),
   database: getEnvVariable('POSTGRES_DB'),
-  port: Number(getEnvVariable('POSTGRES_PORT')) || 5432,
-  api_port: Number(getEnvVariable('PORT')) || 5000, */
+  port: Number(getEnvVariable('POSTGRES_PORT')) || 5432,*/
+  api_port: Number(getEnvVariable('PORT')) || 5000,
   databaseUrl: getEnvVariable('DATABASE_URL'),
 };
 module.exports = {
