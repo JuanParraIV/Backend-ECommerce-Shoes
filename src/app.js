a078a672a8184c5748470117986d26eeb20e5929
JuanParraIV <jmparra1993@gmail.com>
2023-02-10 20:33:08 -0500

    fix: cors credentials in true

--- src/app.js
+++ src/app.js
@@ -12,9 +12,7 @@ const cors = require('cors');
 server.name = 'API';
 
 
-server.use(cors({
-  credentials: true
-}));
+server.use(cors({}));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
